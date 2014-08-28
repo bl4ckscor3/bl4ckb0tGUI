4fdac7ac55a04f3483532c29837dd38926d1f059
bl4ckscor3 <donalddani9612@gmx.de>
2014-08-28 22:23:18 +0200

    not able to resize window

--- bl4ckb0tGUI/src/bl4ckscor3/bot/bl4ckb0tGUI/core/Core.java
+++ bl4ckb0tGUI/src/bl4ckscor3/bot/bl4ckb0tGUI/core/Core.java
@@ -1,15 +1,9 @@
 package bl4ckscor3.bot.bl4ckb0tGUI.core;
 
-import java.awt.Container;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
 
 import org.pircbotx.Configuration;
 import org.pircbotx.PircBotX;
@@ -24,26 +18,25 @@ public class Core
 	public static Gui gui;
 	public static String name;
 	public static NameGui nameGui;
-	
+
 	public static void main(String args[]) throws IOException, IrcException
 	{
 		setupNameGui();
-		createBot();
 	}
 	
 	private static void setupNameGui()
 	{
 		nameGui = new NameGui();
-		
+
 		nameGui.setTitle("Username selection");
 		nameGui.setFont(new Font("Arial", 0, 14));
 		nameGui.setSize(300, 150);
 		nameGui.setLocationRelativeTo(null);
-		nameGui.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		nameGui.setEnabled(true);
+		nameGui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		nameGui.setResizable(false);
 		nameGui.setVisible(true);
 	}
-	
+
 	public static void setupGui()
 	{
 		gui = new Gui();
@@ -53,10 +46,10 @@ public class Core
 		gui.setSize(800, 800);
 		gui.setLocationRelativeTo(null);
 		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		gui.setEnabled(true);
+		gui.setResizable(false);
 		gui.setVisible(true);
 	}
-	
+
 	public static void createBot()
 	{
 		Configuration config = new Configuration.Builder()	
@@ -71,7 +64,7 @@ public class Core
 		.setMessageDelay(500)
 		.buildConfiguration();
 		bot = new PircBotX(config);
-		
+
 		try
 		{
 			bot.startBot();
