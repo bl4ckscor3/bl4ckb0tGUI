094742efe34e25f81446d02eec3f108313fa53cb
bl4ckscor3 <donalddani9612@gmx.de>
2014-09-09 14:58:38 +0200

    now with scrollbar!

--- bl4ckb0tGUI/src/bl4ckscor3/bot/bl4ckb0tGUI/gui/main/tabs/TabOutput.java
+++ bl4ckb0tGUI/src/bl4ckscor3/bot/bl4ckb0tGUI/gui/main/tabs/TabOutput.java
@@ -1,7 +1,11 @@
 package bl4ckscor3.bot.bl4ckb0tGUI.gui.main.tabs;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
 
 import bl4ckscor3.bot.bl4ckb0tGUI.core.Core;
 
@@ -9,13 +13,19 @@ import bl4ckscor3.bot.bl4ckb0tGUI.core.Core;
 public class TabOutput extends JPanel 
 {
 	public static JTextArea area = new JTextArea();
+	private JScrollPane scrollBar = new JScrollPane();
 	
 	public TabOutput()
 	{
 		setLayout(null);
 		
-		area.setBounds(9, 9, 770, 725);
+		area.setWrapStyleWord(true);
+		area.setLineWrap(true); 
 		area.setEditable(false);
-		add(area);
+		scrollBar.setBounds(9, 9, 770, 725);
+		scrollBar.setBorder(BorderFactory.createTitledBorder("Output"));
+		scrollBar.setViewportView(area);
+		scrollBar.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		add(scrollBar);
 	}
 }
